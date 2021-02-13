c34e8ca7e4d36136719a34aa8eb4f7e578f50cf6
PLSetti <50505827+PLSetti@users.noreply.github.com>
2021-02-13 14:05:46 +0100

    Create custom sidebar block partnerzy.php
    custom sidebarblock

---
+++ custom sidebar block partnerzy.php
@@ -0,0 +1,11 @@
+<div id="partner">
+<span>
+<a href="https://www.1shot1kill.pl/"_blank" data-ipstooltip="" _title="Serwery Gier" tabindex="0"><img src="https://www.1shot1kill.pl/r/b500x100.png" alt="!shot!kill" style="width: 390px;"></a>
+</span>
+<span>
+<a href="https://goboosting.pl/" target="_blank" data-ipstooltip="" _title="Gobosting tabindex="0"><img src="https://i.imgur.com/LN7Pq5S.png alt="Goobosting" style="width: 200px;"></a>
+</span>
+<span>
+<a href="https://cssetti.pl/download" title="serwery cs 1.6"><img src="https://static.cssetti.pl/DownloadButtons/6.png"></a>
+</span>
+</div>
